bd3eb2acff34a91935e220a4e9604a17753575cb
Vasanti Suthar <vasantisuthar15@gmail.com>
2021-09-12 21:14:50 +0530

    created express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) => {
+    res.send("welcome ");
+})
+app.listen(3000, () => {
+    console.log("server started at port 3000");
+})
